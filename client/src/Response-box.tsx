ecece9850b70d3d90e90c53c5b2681263c712562
Guilherme Neis <guilhermeneis132@gmail.com>
2023-09-13 22:55:28 -0300

    Correção de erro em que o título do retorno do servidor era sempre erro no front-end

--- client/src/Response-box.tsx
+++ client/src/Response-box.tsx
@@ -1,38 +1,33 @@
 // import React from "react";
-import { Button, Card, ListGroup, ListGroupItem, Spinner } from "react-bootstrap";
+import { Button, Card, ListGroup, ListGroupItem} from "react-bootstrap";
 import { ServerResponse } from "./model/server-response";
 import { FC } from "react";
 
-interface ServerResponseProps{
-  response: ServerResponse;
+interface ServerResponseProps {
+	response: ServerResponse;
 }
 
-export const ResponseBox: FC<ServerResponseProps> = ({response}) => {
-  console.log("messages: "+response);
-  return (
-    <div>
-        {response.validationStatus.length > 0 ? (
-        <ListGroup>
-          {response.validationStatus.map((message, index) => (
-            <ListGroupItem key={index}>
-              <Card>
-                <Card.Body>
-                  <Card.Title>{message.message}</Card.Title>
-                  {message.type === "success" ? (
-                    <Button variant="success">Sucesso</Button>
-                  ) : (
-                    <Button variant="danger">Erro</Button>
-                  )}
-                </Card.Body>
-              </Card>
-            </ListGroupItem>
-          ))}
-        </ListGroup>
-      ) : (
-        <Spinner size="sm" />
-      )}
-    </div>
-  );
+export const ResponseBox: FC<ServerResponseProps> = ({ response }) => {
+	return (
+		<div>
+			<ListGroup>
+				{response.validationStatus.map((message, index) => (
+					<ListGroupItem key={index}>
+						<Card>
+							<Card.Body>
+								<Card.Title>{message.message}</Card.Title>
+								{message.type === "Success" ? (
+									<Button variant="success">Sucesso</Button>
+								) : (
+									<Button variant="danger">Erro</Button>
+								)}
+							</Card.Body>
+						</Card>
+					</ListGroupItem>
+				))}
+			</ListGroup>
+		</div>
+	);
 };
 
 export default ResponseBox;
