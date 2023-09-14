a8f6fa0dc746cee57b1e4ade1393d1b5149287f7
Guilherme Neis <guilhermeneis132@gmail.com>
2023-09-14 00:54:54 -0300

    Alterações no css da página e elaboração de um design mais conciso

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,87 +1,82 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import CsvUploader from './Csv-uploader';
+import { Container, Row, Button } from 'react-bootstrap';
+import CsvUploader from './CsvUploader';
 import { PRArray } from './model/PRArray';
 import { ProductRequest } from './model/ProductRequest';
-import ResponseBox from './Response-box';
+import ResponseBox from './ResponseBox';
 import { ServerResponse } from './model/server-response';
 const strOffset = 24;
 
 const App: React.FC = () => {
-	const [responseMessage, setResponseMessage] = useState<ServerResponse>(new ServerResponse([]));
-	const [buttonDisabled, setButtonDisabled] = useState<boolean>(true);
+    const [responseMessage, setResponseMessage] = useState<ServerResponse>(new ServerResponse([]));
+    const [buttonDisabled, setButtonDisabled] = useState<boolean>(true);
 
-	const handleUpload = async (file: File) => {
-		try {
-			let b = await file.text();
-			b = b.slice(strOffset);
-			const c = b.split("\r\n");
-			let pList = new PRArray();
-			let args: string[];
-			for (let i = 0; i < c.length; i++) {
-				args = c[i].split(",");
-				pList.add(new ProductRequest(args));
-			}
-			const response = await fetch('http://localhost:3000/validate/', {
-				method: 'POST',
-				headers: { "Content-Type": "application/json; charset=utf-8" },
-				body: JSON.stringify(pList.array),
-			});
-			const data = await response.text();
-			buttonState(JSON.parse(data));
-		} catch (error) {
-			console.log(error);
-			setButtonDisabled(true);
-		}
+    const handleUpload = async (file: File) => {
+        try {
+            let b = await file.text();
+            b = b.slice(strOffset);
+            const c = b.split("\r\n");
+            let pList = new PRArray();
+            let args: string[];
+            for (let i = 0; i < c.length; i++) {
+                args = c[i].split(",");
+                pList.add(new ProductRequest(args));
+            }
+            const response = await fetch('http://localhost:3000/validate/', {
+                method: 'POST',
+                headers: { "Content-Type": "application/json; charset=utf-8" },
+                body: JSON.stringify(pList.array),
+            });
+            const data = await response.text();
+            buttonState(JSON.parse(data));
+        } catch (error) {
+            console.log(error);
+            setButtonDisabled(true);
+        }
 
-		function buttonState(data: ServerResponse) {
-			if (data.validationStatus[0].type != "Success") {
-				console.log("TRUE " + JSON.stringify(data));
-				setButtonDisabled(true);
-			} else {
-				console.log("FALSE " + JSON.stringify(data));
-				setButtonDisabled(false);
-			}
-			setResponseMessage(data);
-		}
-	};
+        function buttonState(data: ServerResponse) {
+            if (data.validationStatus[0].type != "Success") {
+                console.log("TRUE " + JSON.stringify(data));
+                setButtonDisabled(true);
+            } else {
+                console.log("FALSE " + JSON.stringify(data));
+                setButtonDisabled(false);
+            }
+            setResponseMessage(data);
+        }
+    };
 
-	const onFinish = async () => {
-		try {
-			const response = await fetch('http://localhost:3000/update/', {
-				method: 'POST',
-			});
-			if (response.ok) {
-				const data = await response.text();
-				setResponseMessage(JSON.parse(data));
-			} else {
-				const data = await response.text();
-				setResponseMessage(JSON.parse(data));
-			}
-		} catch (error) {
-			// setResponseBody("Uknown Error ocurred!")
-		}
-	};
+    const onFinish = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/update/', {
+                method: 'POST',
+            });
+            if (response.ok) {
+                const data = await response.text();
+                setResponseMessage(JSON.parse(data));
+            } else {
+                const data = await response.text();
+                setResponseMessage(JSON.parse(data));
+            }
+        } catch (error) {
+            // setResponseBody("Uknown Error ocurred!")
+        }
+    };
 
-	return (
-		<Container>
-			<Row>
-				<Col>
-					<h1>Upload de CSV</h1>
-					<CsvUploader onUpload={handleUpload} />
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<Button disabled={buttonDisabled} className='bUpdate' onClick={onFinish}>Atualizar</Button>
-					<h2>Resposta do Back-end</h2>
-					<div>
-						<ResponseBox response={responseMessage} />
-					</div>
-				</Col>
-			</Row>
-		</Container>
-	);
+    return (
+        <Container>
+            <Container className='header'>
+                <img className='header_image' src='../common/placeholder_company_logo.jpg' alt='Company Logo'></img>
+                <h2>Upload de CSV</h2>
+            </Container>
+            <Container className='page'>
+                <CsvUploader onUpload={handleUpload} />
+                <Button disabled={buttonDisabled} className='bUpdate' onClick={onFinish}>Atualizar</Button>
+                <ResponseBox response={responseMessage} />
+            </Container>
+        </Container>
+
+    );
 };
 
 export default App;
---
+++ client/src/ResponseBox.tsx
@@ -0,0 +1,33 @@
+// import React from "react";
+import { Button, Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import { ServerResponse } from "./model/server-response";
+import { FC } from "react";
+
+interface ServerResponseProps {
+    response: ServerResponse;
+}
+
+export const ResponseBox: FC<ServerResponseProps> = ({ response }) => {
+    return (
+        <div>
+            <ListGroup>
+                {response.validationStatus.map((message, index) => (
+                    <ListGroupItem key={index}>
+                        <Card>
+                            <Card.Body>
+                                {message.type === "Success" ? (
+                                    <h3>Sucesso</h3>
+                                ) : (
+                                    <h3>Erro</h3>
+                                )}
+                                <Card.Title>{message.message}</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </ListGroupItem>
+                ))}
+            </ListGroup>
+        </div>
+    );
+};
+
+export default ResponseBox;
